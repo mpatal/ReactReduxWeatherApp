7e589206285528b0ba2b6bcfb8dd3cb042cc9659
Marvin <marvin.patal@hotmail.com>
2017-03-08 18:59:34 -0500

    prepare handling of form submission for search bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,33 +3,36 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
 
         this.state = {
-            term : ''
+            term: ''
         };
 
+        //call back that makes a reference to this must be bound
         this.onInputChange = this.onInputChange.bind(this);
     }
 
     onInputChange(event) {
-        console.log(event.target.value);
-        //console.log(`event recived ${event}`);
-        this.setState ({term : event.target.value});
+        this.setState({ term: event.target.value });
+    }
+
+    onFormSubmit(event){
+        event.preventDefault();
+
+        //fetch weather data here
     }
 
     render() {
-        debugger;
         return (
-            <form className="input-group">
-                <input 
-                    className="form-control" 
-                    placeholder="Get a five-day forecast in your favorite cities" 
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input
+                    className="form-control"
+                    placeholder="Get a five-day forecast in your favorite cities"
                     onChange={this.onInputChange}
                     value={this.state.term}
-                    />
+                />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
